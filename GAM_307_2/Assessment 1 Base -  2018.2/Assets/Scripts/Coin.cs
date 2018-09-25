b177c979df3a935460a88c5c1de1f533457faacb
Benjamin Hall <BHall2@myjmc.edu.au>
2018-09-25 16:10:37 +1000

    Random-Coin-Spin-Problem
    Coins default to y 0 position when game starts playing.

--- GAM_307_2/Assessment 1 Base -  2018.2/Assets/Scripts/Coin.cs
+++ GAM_307_2/Assessment 1 Base -  2018.2/Assets/Scripts/Coin.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour {
     [SerializeField]
-    private float rotateSpeed = 1.0f;
+    private float rotateSpeed = 1.0f; //coin spin speed
+
+    [SerializeField]
+    private float floatSpeed = 0.5f; //coins vertical movement speed
+
+    [SerializeField]
+    private float movementDistance = 0.5f; //coins movement distance
+
+    private float startingY; //coins Y starting position gives us a value so we know how far it strays from inital position 
+    private bool isMovingUp = true; // 
+
 
     // Use this for initialization
     void Start() {
-        StartCoroutine(Spin ());
+         StartCoroutine(Spin ());
+         StartCoroutine(Float());
+
+        startingY = transform.position.y;
+
+        transform.Rotate(transform.up, Random.Range(0f, 360f));
+       
+        
     }
     private IEnumerator Spin()
         {
@@ -18,10 +35,25 @@ public class Coin : MonoBehaviour {
                 yield return 0;
             }
         }
+
+    private IEnumerator Float()
+    {
+        while (true)
+        {
+            float newY = transform.position.y + (isMovingUp ? 1 : -1) * 2 * movementDistance * floatSpeed * Time.deltaTime;
+            if (newY > startingY + movementDistance)
+            { newY = startingY + movementDistance;
+                isMovingUp = false;
+            }
+            else if (newY < startingY)
+            { newY = startingY;
+                isMovingUp = true;
+            }
+
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+            yield return 0;
+        }
+    }
 	
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
