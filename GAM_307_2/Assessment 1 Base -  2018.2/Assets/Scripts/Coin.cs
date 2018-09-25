c5f51128fd4afacaa43e119b49296062765b8109
Benjamin Hall <BHall2@myjmc.edu.au>
2018-09-25 14:53:43 +1000

    Coin Script 1 Commit

---
+++ GAM_307_2/Assessment 1 Base -  2018.2/Assets/Scripts/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+    [SerializeField]
+    private float rotateSpeed = 1.0f;
+
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(Spin ());
+    }
+    private IEnumerator Spin()
+        {
+            while (true)
+            {
+               transform.Rotate (transform.up, 360 *rotateSpeed* Time.deltaTime);
+                yield return 0;
+            }
+        }
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
