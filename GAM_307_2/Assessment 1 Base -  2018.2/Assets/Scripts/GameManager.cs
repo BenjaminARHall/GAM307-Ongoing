8393cec9bcefef891077234165d49e15d50a284d
Benjamin Hall <BHall2@myjmc.edu.au>
2018-10-02 15:12:52 +1000

    Singleton UI Timer
    Added a timer that works with the singleton script automatically

--- GAM_307_2/Assessment 1 Base -  2018.2/Assets/Scripts/GameManager.cs
+++ GAM_307_2/Assessment 1 Base -  2018.2/Assets/Scripts/GameManager.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager> {
 
+    private float _timeRemaining;
+
+    private float maxTime = 2 * 60; //in seconds
+
+    public float TimeRemaining
+    {
+        get { return _timeRemaining; }
+        set { _timeRemaining = value; }
+
+    }
+
 	// Use this for initialization
 	void Start () {
-		
+        TimeRemaining = maxTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		TimeRemaining -= Time.deltaTime;
+
+        if (TimeRemaining <= 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+            TimeRemaining = maxTime;
+        }
 	}
 }
