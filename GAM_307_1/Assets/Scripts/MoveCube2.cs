045f53342aca8b7e364432fcc9c75bf370489ee2
Benjamin Hall <BHall2@myjmc.edu.au>
2018-10-02 10:36:53 +1000

    Coroutines week 2
    Learning Coroutines and yield commands

---
+++ GAM_307_1/Assets/Scripts/MoveCube2.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube2 : MonoBehaviour {
+
+    public GameObject cube;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StopAllCoroutines();
+            StartCoroutine(MoveCube());
+            Debug.Log("Started");
+        }
+	}
+
+    IEnumerator MoveCube()
+    {
+        for(int i = 0; i < 300; i++ )
+        {
+            cube.transform.Translate(Vector3.forward * Time.deltaTime);
+            
+            yield return null;
+    }
+        yield return new WaitForSeconds(1);
+
+        for (int i = 0; i < 300; i++)
+        {
+            cube.transform.Translate(Vector3.left * Time.deltaTime);
+         
+            yield return null;
+        }
+        yield return new WaitForSeconds(1);
+
+        for (int i = 0; i < 300; i++)
+        {
+            cube.transform.Translate(Vector3.back * Time.deltaTime);
+       
+            yield return null;
+        }
+        yield return new WaitForSeconds(1);
+
+        for (int i = 0; i < 300; i++)
+        {
+            cube.transform.Translate(Vector3.right * Time.deltaTime);
+           
+            yield return null;
+        }
+        yield return new WaitForSeconds(1);
+
+        StartCoroutine(MoveCube());
+
+        Debug.Log("Finished");
+    }
+
+}
---
+++ GAM_307_1/Assets/Scripts/ScriptWeek2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptWeek2 : MonoBehaviour {
+
+	// Use this for initialization
+	/*void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Do something before the coroutine");
+            StartCoroutine(TestText());
+            Debug.Log("Do something after the coroutine");
+            Debug.Log("Do something after the coroutine");
+            Debug.Log("Do something after the coroutine");
+            Debug.Log("Do something after the coroutine");
+            Debug.Log("Do something after the coroutine");
+        }
+	}
+
+   
+    IEnumerator TestText()
+    {
+        yield return null;
+        Debug.Log("Frame 0");
+        yield return null;
+        Debug.Log("Frame 1");
+        yield return null;
+        Debug.Log("Frame 2");
+        yield return null;
+        Debug.Log("Frame 3");
+    }*/
+}
