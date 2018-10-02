fb613ad56e4b9b753ad8ebd42cec7a2afdbc8009
Benjamin Hall <BHall2@myjmc.edu.au>
2018-10-02 14:47:21 +1000

    Singelton and Game Manager
    Learning Singleton and applying it to a game manager

--- GAM_307_2/Assessment 1 Base -  2018.2/Assets/Scripts/Singleton.cs
+++ GAM_307_2/Assessment 1 Base -  2018.2/Assets/Scripts/Singleton.cs
@@ -10,9 +10,32 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
     {
         get
         {
+            if (_instance ==null)
+            {
+                _instance = GameObject.FindObjectOfType<T>();
 
+                if (_instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    _instance = singleton.AddComponent<T>();
+                }
+            }
+            return _instance;
         }
+
     }
 
+    public virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
 }
