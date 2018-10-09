201c4a7a109df38b34c3c425f532865ab00b2df2
Benjamin Hall <BHall2@myjmc.edu.au>
2018-10-09 14:47:19 +1100

    Added player death and restart

---
+++ GAM_307_2/Assessment 1 Base -  2018.2/Assets/Scripts/DamagePlayerEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayerEvent : MonoBehaviour {
+
+    public delegate void DamagePlayerAction(GameObject player);
+    public static event DamagePlayerAction OnDamagePlayer;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (!GetComponentInParent<Enemy> () .isDead)
+        {
+            if (collider.gameObject.tag == "Player")
+            {
+                if (OnDamagePlayer != null)
+                {
+                    OnDamagePlayer(collider.gameObject);
+                }
+            }
+        }
+    }
+}
--- GAM_307_2/Assessment 1 Base -  2018.2/Assets/Scripts/GameManager.cs
+++ GAM_307_2/Assessment 1 Base -  2018.2/Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : Singleton<GameManager> {
 
  private float maxTime = 2 * 60; //in seconds
 
+    private int maxHealth = 5;
+
     private int _numCoins;
 
     public int NumCoins
@@ -15,7 +17,15 @@ public class GameManager : Singleton<GameManager> {
         get { return _numCoins; }
         set { _numCoins = value; }
     }
-   
+
+    private float _playerHealth;
+
+    public float PlayerHealth
+    {
+        get { return _playerHealth; }
+        set { _playerHealth = value; }
+    }
+
 
     public float TimeRemaining
     {
@@ -24,9 +34,19 @@ public class GameManager : Singleton<GameManager> {
 
     }
 
-	// Use this for initialization
-	void Start () {
+    private void OnEnable()
+    {
+        DamagePlayerEvent.OnDamagePlayer += DecrementPlayerHealth;
+    }
+
+    private void OnDisable()
+    {
+        DamagePlayerEvent.OnDamagePlayer -= DecrementPlayerHealth;
+    }
+    // Use this for initialization
+    void Start () {
         TimeRemaining = maxTime;
+        PlayerHealth = maxHealth;
 	}
 	
 	// Update is called once per frame
@@ -35,8 +55,28 @@ public class GameManager : Singleton<GameManager> {
 
         if (TimeRemaining <= 0)
         {
+            Restart();
+        }
+    
+	}
+
+    private void DecrementPlayerHealth (GameObject player)
+    {
+        PlayerHealth--;
+
+        if (PlayerHealth <= 0)
+        {
+            Restart();//Restart game
+        }
+    }
+
+   public void Restart()
+    {
+      
+        {
             Application.LoadLevel(Application.loadedLevel);
             TimeRemaining = maxTime;
+            PlayerHealth = maxHealth;
         }
-	}
+    }
 }
